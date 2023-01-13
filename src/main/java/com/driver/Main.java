8cf737de6d839e3abf507f081e72c9b0d9fd10d0
Sowndharya <sowndharyaktr@gmail.com>
2023-01-13 07:33:06 +0530

    first encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly RW=new RWOnly();
+       // RW.name="value";
+       // System.out.println(RW.name);
+        RW.setName("value");
+        RW.getName();
+    }
 }
